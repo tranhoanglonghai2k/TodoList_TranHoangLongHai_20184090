cb83fc38ccd1566911f6dbe31af4699492aacc5f
Hai Tran <tranhoanglonghai2k@gmail.com>
2022-06-02 12:18:43 +0000

    Add Tabs

--- itss_std_react_todo_k63/src/components/Todo.js
+++ itss_std_react_todo_k63/src/components/Todo.js
@@ -26,6 +26,14 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  
+  const [filter, setFilter] = React.useState('ALL');
+  
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
 
   const handleCheck = checked => {
     const newItems = items.map(item => {
@@ -43,17 +51,23 @@ function Todo() {
     putItems([...items, {key: getKey(), text, done: false}]);
   };
 
+  const handleFilterChange = value => setFilter(value);
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
-      {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem key={item.value} item={item} onCheck={handleCheck} />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
