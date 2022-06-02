7bfc673cfeb3871eb822f08a2a7e2f507765418a
Hai Tran <tranhoanglonghai2k@gmail.com>
2022-06-02 11:50:16 +0000

    Create by other components

--- itss_std_react_todo_k63/src/components/Todo.js
+++ itss_std_react_todo_k63/src/components/Todo.js
@@ -33,10 +33,7 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+        <TodoItem key={item.value} item={item} />
       ))}
       <div className="panel-block">
         {items.length} items
